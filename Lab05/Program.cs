a8f0f2413b29f5072949bf0e3020228f88504930
DmytryPan <panikhidin@sfedu.ru>
2024-11-03 19:03:10 +0300

    lab05 initial commit

---
+++ Lab05/Program.cs
@@ -0,0 +1,29 @@
+namespace Lab05
+{
+    class Fact
+    {
+
+    }
+
+    class Rule
+    {
+
+    }
+
+
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+            
+        }
+    }
+}
